9f58a661229ac6c1ed3569cfb8841e2799da4745
yankee624 <35090493+yankee624@users.noreply.github.com>
2018-04-15 14:00:11 +0900

    Create Finding the percentage.py

---
+++ hackerrank/practice_python/Finding the percentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    
+    query_name = input()
+    s = student_marks[query_name]
+    avg = round(sum(s)/len(s),2)
+    print('{0:.2f}'.format(avg))
