7b3d8a8b9faff549531885622f144d155dcc8e60
yankee624 <kichang96@snu.ac.kr>
2021-10-16 00:31:04 +0900

    211016

--- BaekJoon Online Judge/Backtracking/9663.py
+++ BaekJoon Online Judge/Backtracking/9663.py
@@ -1,3 +1,6 @@
+# Faster but still timeout:
+# For each row, find which column queen can be placed.
+# Check if each column is possible on-demand
 N = int(input())
 
 cnt = 0
@@ -25,3 +28,49 @@ def nqueen(n): # n: row number
 
 nqueen(0)
 print(cnt)
+
+
+
+# Very slow method: Whenever put a queen, block the places where the queen can reach
+# N = int(input())
+#
+# answer = 0
+# board = [[0 for _ in range(N)] for _ in range(N)]
+#
+#
+# def nqueen(idx, cnt):
+#     if cnt == N:
+#         global answer
+#         answer += 1
+#         return
+#     if idx == N * N:
+#         return
+#
+#     row = idx // N
+#     col = idx % N
+#     if board[row][col] == 0:
+#         # put queen
+#         block_idxs = {(row, col)}
+#         for r in range(N):
+#             if board[r][col] == 0:
+#                 block_idxs.add((r,col))
+#             c = r - row + col
+#             if 0 <= c < N and board[r][c] == 0:
+#                 block_idxs.add((r, c))
+#             c = -r + row + col
+#             if 0 <= c < N and board[r][c] == 0:
+#                 block_idxs.add((r, c))
+#         for c in range(N):
+#             if board[row][c] == 0:
+#                 block_idxs.add((row, c))
+#
+#         for (r, c) in block_idxs:
+#             board[r][c] = 1
+#         nqueen(idx+1, cnt+1)
+#         for (r, c) in block_idxs:
+#             board[r][c] = 0
+#     nqueen(idx+1, cnt)
+#
+# nqueen(0, 0)
+# print(answer)
+
